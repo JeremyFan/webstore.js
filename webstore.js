8c5846a2aea298f5b0dad758fca82f5049922056
JeremyFan <jeremyfan_14@live.com>
2013-11-22 11:41:16 +0800

    Update webstore.js

--- webstore.js
+++ webstore.js
@@ -1,4 +1,4 @@
-﻿(function (window) {
+(function (window) {
     // 定义webstore支持的属性/方法
     var set, get;
 
@@ -13,19 +13,30 @@
     }
         // 支持addBehavior的浏览器
     else if (typeof window.document.documentElement.addBehavior == 'object') {
-        // 创建存储元素
-        var store = document.createElement('div');
-        store.addBehavior("url('#default#userData')");
         // 存储空间名
         var storespace = 'StoreSpace';
+        var getStore = function () {
+            if (window.store)
+                return window.store;
+            // 创建存储元素
+            var store = document.createElement('div');
+            window.document.body.appendChild(store);
+            store.addBehavior('#default#userData');
+            // 把store添加到window
+            window.store = store;
+
+            return window.store;
+        }
 
         set = function (key, value) {
+            var store = getStore();
             // 写入数据
             store.setAttribute(key, value);
             // 保存到storespace
             store.save(storespace);
         };
         get = function (key) {
+            var store = getStore();
             // 获取storespace中的所有数据
             store.load(storespace);
             // 读取指定数据
@@ -34,7 +45,7 @@
 
     }
     else {
-        throw new Error("Sorry, your browser doesn't support web storage.");
+        throw new Error("Sorry, your browser doesn't support webstore.js");
     }
 
     // 创建一个临时的webstore对象
@@ -46,5 +57,8 @@
     if (typeof webstore.get == 'function' &&
         typeof webstore.set == 'function') {
         window.webstore = webstore;
+    }
+    else {
+        throw new Error("The object 'webstore' has not been successfully created.")
     }
 })(window);
