84ffcd44ef6c8aa294897de3624a22eb588af0d9
JeremyFan <jeremyfan_14@live.com>
2013-11-25 18:31:12 +0800

    Update webstore.js

--- demo/webstore.js
+++ demo/webstore.js
@@ -1,21 +1,21 @@
-(function(window) {
+(function (window) {
     // 定义webstore支持的属性/方法
     var set, get;
 
     // 支持localStorage的浏览器
-    // if (typeof window.localStorage == 'object') {
-    //     set = function(key, value) {
-    //         window.localStorage.setItem(key, value);
-    //     };
-    //     get = function(key) {
-    //         return window.localStorage.getItem(key);
-    //     };
-    // }
-    // 支持addBehavior的浏览器
-    if (typeof window.document.documentElement.addBehavior == 'object') {
+    if (typeof window.localStorage == 'object') {
+        set = function (key, value) {
+            window.localStorage.setItem(key, value);
+        };
+        get = function (key) {
+            return window.localStorage.getItem(key);
+        };
+    }
+        // 支持addBehavior的浏览器
+    else if (true) {
         // 存储空间名
         var storespace = 'StoreSpace';
-        var setStore = function() {
+        var setStore = function () {
             if (window.store)
                 return;
             // 创建存储元素
@@ -25,29 +25,22 @@
             // 把store添加到window
             window.store = store;
         }
+        set = function (key, value) {
+            setStore();
+            // 写入数据
+            store.setAttribute(key, value);
+            // 保存到storespace
+            store.save(storespace);
 
-        set = function(key, value) {
-            try {
-                setStore();
-                // 写入数据
-                store.setAttribute(key, value);
-                // 保存到storespace
-                store.save(storespace);
-            } catch (error) {
-                alert(error.message);
-            }
         };
-        get = function(key) {
-            try {
-                setStore();
-                // 获取storespace中的所有数据
-                store.load(storespace);
-                // 读取指定数据
-                return store.getAttribute(key);
-            } catch (error) {
-                alert(error.message);
-            }
-        };
+        get = function (key) {
+            //debugger;
+            setStore();
+            // 获取storespace中的所有数据
+            store.load(storespace);
+            // 读取指定数据
+            return store.getAttribute(key);
+        }
 
     } else {
         throw new Error("Sorry, your browser doesn't support webstore.js");
